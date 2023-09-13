28afdd6c3beeaacba4a5d63e0a3e2de3a2c2348d
parkjjung79 <parkjjung79@naver.com>
2023-09-13 16:31:37 +0900

    for_in문 / nav06

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -13,11 +13,45 @@ const domFn = {
   addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
 }; /////// domFn 객체 /////////////
 
-
 // 1. 구현 요구사항
 // GNB메뉴의 데이터를 모두 html DOM으로 구조화하여
 // 화면에 출력한다!
 
 // 2. 대상선정: .gnb
-const gnbBox = domFn.qs('.gnb');
-console.log('대상:',gnbBox);
+const gnbBox = domFn.qs(".gnb");
+console.log("대상:", gnbBox);
+
+// 3. 객체 데이터 html 코드만들기
+let hcode = '';
+
+
+for (let x in mdata) {
+  console.log("속성명:", x);
+  hcode +=
+  `
+   
+   <ul>
+   <li>
+       <a href="">${x}</a>
+       <div class="smenu">
+           <aside class="smbx">
+               <h2>
+                   <div class="stit">2차</div>
+                   <a href="">전체보기</a>
+                   <div class="swrap">
+                       <dl>
+                           <dt></dt>
+                           <dd><a href="">요기</a></dd>
+                        </dl>
+                    </div>
+                </h2>
+            </aside>
+        </div>
+    </li>
+  </ul>
+
+   `;
+} ////////// for in ///////////
+
+// 확인
+console.log('코드:',hcode);
