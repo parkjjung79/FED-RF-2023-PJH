e048bc7f16dde467063e5d4d081ea8d6c438e0d0
parkjjung79 <parkjjung79@naver.com>
2023-09-21 14:44:31 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -6,7 +6,7 @@ import dFn from "./dom.js";
 // 부드러운 스크롤 모듈
 import { startSS, setPos } from "./smoothScroll23.js";
 // 데이터모듈
-import { gridData, gnbData } from "./data_drama.js";
+import { gridData, gnbData, previewData } from "./data_drama.js";
 
 // 부드러운 스크롤 적용
 startSS();
@@ -167,3 +167,81 @@ function outFn() {
   // 서브메뉴 박스 높이값 0만들기!
   dFn.qsEl(this, ".smenu").style.height = "0px";
 }
+
+
+
+
+
+
+
+/////////////////////////////////////////////
+/////// 인트로 동영상 클릭시 플레이하기 ///////
+// 대상 :.intro-mv-img
+// 이벤트: click
+// -> 가상요소 플레이버튼 클릭시
+// 이벤트 버블링으로 본 박스가 반응함!!!!!
+
+// 1. 대상선정하기
+const mvBox = dFn.qs('.intro-mv-img');
+
+// 2. 이벤트 설정하기
+dFn.addEvt(mvBox,'click',showMv);
+
+// 상태변수 : 이벤트(한번만 실행하기위한 변수)
+let stsShowMv = 0;
+
+// 3. 함수만들기
+function showMv(){
+  if(stsShowMv) return; // 돌아가!
+  stsShowMv = 1; // 한번만 실행
+ console.log('보여줘!');
+ // 동영상 넣기
+ // 대상 : 나자신(.intro-mv-img)
+ this.innerHTML = `
+  <video src='./images/pc/intro_mv.mp4' controls autoplay></video>
+ `;
+
+ // 가상요소 플레이버튼 없애기위해 .off지우기
+ this.classList.remove('off');
+
+
+} ///////////// showMv함수 ////////////////
+
+
+
+////////////////////////////////////////////////////
+// 오름차순 데이터를 내림차순으로 변경하여 화면에 뿌리기
+
+// 1. 데이터 정렬 변경하기
+let preNewData = 
+previewData.sort((x,y)=>{
+ // x,y는 배열값 앞뒤를 계속 가지고 들어옴!
+ // 변수 -> 문자형을 숫자형으로 바꿈!
+ // 배열값 중 idx속성값을 가져와서 숫자형 변환!후 'Number'를 추가!
+ // 사용은 Number();
+ let a = Number(x.idx);
+ let b = Number(y.idx);
+
+ // 배열 순서변경인 메서드인 sort() 내부에 retun값을
+ // 사용하여 순서를 변경한 새로운 배열을 만들어준다!
+ return a == b ? 0 :( a > b? -1:1);
+ // 비?집:(눈?집:놀이동산)
+
+});
+console.log(preNewData);
+
+// 2. 화면 대상에 태그 만들어 넣기
+// 2. 대상선정 : .preview-box>div
+const preBox = dFn.qsa('.preview-box>div');
+console.log(preBox);
+
+// 3. 대상을 순회하여 태그 넣기
+// 데이터 : 역순정렬을 한 미리보기 데이터 넣기
+preBox.forEach((ele,idx)=>{
+  ele.innerHTML = `
+    <div>
+    <h3>${preNewData[idx].title}</h3>
+    <p>${preNewData[idx].story}</p>
+    </div>
+  `;
+}); //////// forEach ///////////////////////
