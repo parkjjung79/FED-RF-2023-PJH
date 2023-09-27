575ef67d9f481cc63e67126db65ab9a065c6ede1
parkjjung79 <parkjjung79@naver.com>
2023-09-27 15:14:12 +0900

    ss

--- 003.JS학습/01.JS실험실/JS/cube2.js
+++ 003.JS학습/01.JS실험실/JS/cube2.js
@@ -11,19 +11,23 @@
     - 단위각도 : 360도 / 9개 = 40도
     - css 이징적용 : ease-out
 
+    [캐릭터별 고유번호 만들기]
+    - 전역변수를 만들어 고유번호 저장
+    - 스크롤 방향에 따른 고유번호 증감
+    - 고유번호를 순화하여 항상 고정번호 만들기
+    -> 9개니까 0~8까지의 고유번호가 순환한다!
+
 *************************************/
 
-// DOM 함수 객체 //////////////
-const domFn = {
-    // 요소선택함수 ////////
-    qs: (x) => document.querySelector(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsa: (x) => document.querySelectorAll(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
-    
-    // 이벤트셋팅함수
-    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-}; /////// domFn 객체 /////////////
+// DOM메서드 모듈
+import domFn from "./dom.js";
+
+// 데이터 제이슨 불러오기 : 어서써 타입 제이슨!
+import mvData from './data_moving.json' assert{type:'json'};
+
+
+// console.log(mvData);
+
 
 // 0. 변수셋팅
 // 단위각도
@@ -31,13 +35,23 @@ const DEG = 40;
 // 광휠상태변수(0-허용,1-금지)
 let stsWheel = 0;
 // 휠제어시간
-const TIME_WHEEL = 250;
+const TIME_WHEEL = 120;
 // 휠단위수(휠할때 증감하는 수)
 let numWheel = 0;
+// 캐릭터 고유번호
+let catNum = 0;
+// 캐릭터 번호한계수(9개니까 한계수 8)
+const LIMIT_CNT = 8;
+// 캐릭터 정보함수 호출 타임아웃용 변수
+let autoT;
+
+
 
 // 1. 대상선정 : .cube
 const cube = domFn.qs('.cube');
-console.log('대상:',cube);
+// 정보표시박스 : .cat-info
+const infoBox = domFn.qs('.cat-info');
+console.log('대상:',cube, infoBox);
 
 // 2. 이벤트 설정하기
 domFn.addEvt(window,'wheel',rotateMem);
@@ -57,22 +71,52 @@ function rotateMem(){
     
     // 2. 방향에 따른 휠단위수 증감하기!
     if(delta<0){
-        // 휠단위수 증가!
-        numWheel--
+        // 휠단위수 감소
+        numWheel--;
+
+        // 캐릭터 고유번호 증가(한계수에서 0으로 순환!)
+        catNum++;
+        if(catNum>LIMIT_CNT)catNum=0;
     }
     else{
-        // 휠단위수 감소
+        // 휠단위수 증가!
         numWheel++;
+
+        // 캐릭터 고유번호 감소(한계수에서 끝번호로 순환!)
+        catNum--;
+        if(catNum < 0) catNum = LIMIT_CNT;
     }
 
     // 3. 회전대상요소에 각도 적용하기
     // 적용각도 = 단위각도 * 휠단위수
     cube.style.transform = `rotateY(${numWheel*DEG}deg)`;
-
-
-
-
-    // 호출확인
-    console.log('휠!!!!!!!!!!!!!!!!',delta,numWheel);
     
+    // 4. 캐릭터가 정지한지 0.5초 후 정보를 셋팅함!
+    // 타임아웃 실행 쓰나미 방지 지우기
+    clearTimeout(autoT);
+    // 0.5초후 캐릭터 정보 셋팅함수 호출!
+    autoT = setTimeout(showInfo,500);
+
+    // 5. 기본 캐릭터정보 박스 클래스on지우기
+    infoBox.classList.remove('on');
+
 } ////////// rotateMem 함수 ///////////
+
+
+/********************************************* 
+ 함수명 : showInfo
+ 기능 : 캐릭터 정보를 화면에 표시한다.
+ *********************************************/
+function showInfo(){
+    console.log('보여줄게...', mvData[catNum]);
+
+    // 정보표시박스에 h2,p요소 정보와 함께 넣기
+    infoBox.innerHTML = `
+        <h2>${mvData[catNum].name}</h2>
+        <p>${mvData[catNum].desc}</p>
+    `;
+    // 등장 클래스 on주기
+    infoBox.classList.add('on');
+
+} //////////// showInfo 함수 //////////////
+
