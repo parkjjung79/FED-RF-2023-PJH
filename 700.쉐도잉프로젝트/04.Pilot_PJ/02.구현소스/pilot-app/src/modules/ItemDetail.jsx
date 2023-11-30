33312167c7c8617f6784059ff41d6491d90413c0
parkjjung79 <parkjjung79@naver.com>
2023-11-30 12:34:05 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
@@ -1,25 +1,80 @@
 // 상품상세보기 컴포넌트
 
 // 신상품 데이터 가져오기
+import { useEffect } from "react";
 import { sinsangData } from "../data/sinsang";
 
-export function ItemDetail({cat,goods}) {
-  // goods - 상품 아이템정보(속성코드)
+import $ from 'jquery';
 
-  // 선택데이터
-  const selData = sinsangData[cat][goods];
+export function ItemDetail({cat,goods}) {
+  // cat - 카테고리명(men/women/style)
+  // goods - 상품 아이템정보(속성코드: m1,m2,....)
+
+  // 선택데이터 : 전체데이터[분류명][상품코드].split('^')
+  // -> 개별상품 배열이 된다!
+  // [상품명, 상품코드, 가격]
+  const selData = sinsangData[cat][goods].split('^');
+  // const selData = sinsangData[cat][goods].split('^');
   console.log('선택데이터:',selData);
 
+  // 닫기함수 /////
+  const closebox = (e) => {
+    e.preventDefault();
+    $('.bgbx').slideUp(400);}
+
+  // 랜더링후 실행구역 ///////////////
+  useEffect(()=>{
+    // 숫자출력 input
+    const sum = $('#sum');
+
+    // 수량증감 이미지버튼
+    const numBtn = $('.chg_num img');
+    
+    // 수량 증감 함수 //////////////////
+    numBtn.click(e=>{
+      // 이미지 순번
+      let seq = $(e.currentTarget).index();
+      // 기존값 읽기 
+      let num = Number(sum.val());
+      // 윗버튼은 ++, 아랫버튼은 --
+      seq?num--:num++;
+      // 한계값
+      if(num<1) num=1;
+      console.log('순번:',seq,num);
+      // 증감 반영
+      sum.val(num);
+      // 총합계 반영
+      // 기본값 : selData[2]
+      // 출력박스 : #total
+      $("#total").text(addComma(selData[2]*num)+'원');
+    })
+
+  },[]); ////////// 한번만 실행 //////////
+
+  // 리랜더링 실행구역 ////////////////
+  useEffect(()=>{
+    // 수량초기화
+    $("#sum").val('1');
+    // 총합계초기화
+    $("#total").text(addComma(selData[2]+'원'));
+  }); ///////////// useEffect /////////////
+
+  //정규식함수(숫자 세자리마다 콤마해주는 기능)
+  function addComma(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+    
   // 리턴코드 ////////////////////////
   return (
     <>
-      <a href="#" className="cbtn">
+      <a href="#" className="cbtn" onClick={closebox}>
         <span className="ir">닫기버튼</span>
       </a>
       <div id="imbx">
         <div className="inx">
           <section className="gimg">
-            <img src="./images/goods/men/m3.png" alt="큰 이미지" />
+            <img src={"./images/goods/"+cat+"/"+goods+".png"} alt="큰 이미지" />
             <div className="small">
               <a href="#">
                 <img src="./images/goods/men/m1.png" alt="썸네일 이미지" />
@@ -38,7 +93,7 @@ export function ItemDetail({cat,goods}) {
                 <li>
                   <img src="./images/dx_ico_new-28143800.gif" alt="new버튼" />
                 </li>
-                <li id="gtit">상품명: [남성]빅로고 컬러 블럭 PQ 티셔츠</li>
+                <li id="gtit">상품명: {selData[0]}</li>
                 <li>
                   <img src="./images/icon_type02_social01.gif" alt="페이스북" />
                   <img src="./images/icon_type02_social02.gif" alt="트위터" />
@@ -47,7 +102,7 @@ export function ItemDetail({cat,goods}) {
                 </li>
                 <li>
                   <span>판매가</span>
-                  <span id="gprice">89,000원</span>
+                  <span id="gprice">{addComma(selData[2])}원</span>
                 </li>
                 <li>
                   <span>적립금</span>
@@ -70,7 +125,7 @@ export function ItemDetail({cat,goods}) {
                   <span>상품코드</span> <span id="gcode">DMTS7G731-WH</span>
                 </li>
                 <li>
-                  <span>사이즈</span> <span>95 100 105 110</span>
+                  <span>사이즈</span> <span>{selData[1]}</span>
                 </li>
                 <li>
                   <span>구매수량</span>
@@ -89,7 +144,8 @@ export function ItemDetail({cat,goods}) {
                   <span>권장계절</span> <span>여름</span>
                 </li>
                 <li className="tot">
-                  <span>총합계</span> <span id="total">13,000</span>
+                  <span>총합계</span>
+                  <span id="total">{addComma(selData[2])}원</span>
                 </li>
               </ol>
             </div>
