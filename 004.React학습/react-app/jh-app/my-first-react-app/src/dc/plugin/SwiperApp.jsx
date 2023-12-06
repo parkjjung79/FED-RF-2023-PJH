4b8063c8a249c8442fe81ac069564b988ce29c7d
parkjjung79 <parkjjung79@naver.com>
2023-12-06 18:17:44 +0900

    ss

--- 004.React학습/react-app/jh-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/jh-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -15,8 +15,12 @@ import "./css/swiper.css";
 // 사용할 스와이퍼 모듈을 불러온다
 // (여기서는 페이지네이션, 네비게이션,자동넘김)
 import { Pagination,Navigation,Autoplay } from "swiper/modules";
+import { useRef } from "react";
 
 export function SwiperApp() {
+
+
+
   // 불러올 이미지 리스트
   const imgArr = [
     "dcm43",
@@ -30,6 +34,9 @@ export function SwiperApp() {
     "dcm35",
   ];
 
+  // useRef로 스와이퍼 객체를 받아온다!
+  const myRef = useRef(null);
+
   // 플레이/멈춤기능 함수
   const stopPlay = () => {
     console.log('멈추거나 플레이!');
@@ -40,6 +47,8 @@ export function SwiperApp() {
   return (
     <> 
       <Swiper
+      /* ref 속성에 useRef 할당변수를 넣어서 외부에 연결함 */
+      ref={myRef}
         slidesPerView={3}
         spaceBetween={30}
         pagination={{
