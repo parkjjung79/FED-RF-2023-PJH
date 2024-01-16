4401a5240cd8820b4cd1d2b5f6d6c7dbc26549fb
parkjjung79 <parkjjung79@naver.com>
2024-01-16 18:09:06 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -19,26 +19,36 @@ export function MainCont() {
     // 랜더링 후 한번만 적용!
     // console.log("랜더링OK!");
     
-    // 스크롤바 없애기
-    $('html,body').css({overflow:'hidden'});
+    // 모바일은 가로크기 800px 이하
+    if($(window).width()>800) {
+       
+      // 스크롤바 없애기
+      $('html,body').css({overflow:'hidden'});
+  
+      // (((중요!!!))) /////////////////////////////
+      // 특정이벤트를 설정해제하고자 할때
+      // 반드시 그 이벤트 설정은 JS파일 내부에서 하지 말고
+      // 리액트 함수에서 JS함수를 호출하는 형태로해야
+      // 해제 메서드인 removeEventListener 가 유효함!
+  
+      // 자동스크롤 이벤트 설정하기 /////
+      window.addEventListener('wheel',wheelFn);
+  
+      // 메뉴+인디케이터 이벤트 기능설정함수 호출 ////
+      evtFn();
+  
+      // 초기화 함수 호출 : 조건(모바일 아닐때)
+      initSet();
+  
+      // 페이지번호 초기화 호출
+      zeroPno();
 
-    // (((중요!!!))) /////////////////////////////
-    // 특정이벤트를 설정해제하고자 할때
-    // 반드시 그 이벤트 설정은 JS파일 내부에서 하지 말고
-    // 리액트 함수에서 JS함수를 호출하는 형태로해야
-    // 해제 메서드인 removeEventListener 가 유효함!
+      
 
-    // 자동스크롤 이벤트 설정하기 /////
-    window.addEventListener('wheel',wheelFn);
 
-    // 메뉴+인디케이터 이벤트 기능설정함수 호출 ////
-    evtFn();
+    } //////// 모바일 아닐때만 실행 ////////
 
-    // 초기화 함수 호출
-    initSet();
 
-    // 페이지번호 초기화 호출
-    zeroPno();
 
     //드래그배너 호출
     dragBanner();
@@ -48,6 +58,7 @@ export function MainCont() {
       console.log('난 소멸했어~!');
 
       // 이 페이지에서만 필요했던 자동스크롤 이벤트 해제!
+
       window.removeEventListener('wheel',wheelFn);
 
       // 메인 페이지에만 사용되는 로고클릭시 상단이동 이벤트 해제
