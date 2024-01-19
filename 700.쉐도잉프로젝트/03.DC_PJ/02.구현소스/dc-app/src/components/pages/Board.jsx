96ea1ab9620034d859ccdb49aa572e1477d68114
parkjjung79 <parkjjung79@naver.com>
2024-01-19 16:07:50 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -5,7 +5,6 @@ import { Fragment, useContext, useEffect, useRef, useState } from "react";
 import "../../css/board.css";
 import "../../css/board_file.css";
 
-
 // 컨텍스트 API 불러오기
 import { dcCon } from "../modules/dcContext";
 
@@ -67,7 +66,6 @@ export function Board() {
   const pgBlock = 7;
   // 1-2. 페이징의 페이지 단위수 : 페이징 표시 개수
   const pgPgBlock = 4;
-  
 
   // 2. 전체 레코드수 : 배열데이터 총개수
   const totNum = orgData.length;
@@ -102,6 +100,14 @@ export function Board() {
   const firstSts = useRef(true);
   // 주의: 참조변수는 최초 랜더링시에만 초기값 셋팅되고
   // 리랜더링시엔 다시 셋팅되지 않는다!
+  
+  // 7. 파일저장변수(참조변수)
+  const uploadFile = useRef(null);
+  // 파일저장변수 업데이트 함수
+  // -> AttachBox컴포넌트 속성으로 내려보냄!
+  const updateFileInfo = x => uploadFile.current = x;
+
+
 
   // 리랜더링 루프에 빠지지 않도록 랜더링후 실행구역에
   // 변경코드를 써준다! 단, logSts에 의존성을 설정해준다!
@@ -157,7 +163,7 @@ export function Board() {
     // 내림차순 정렬적용하기
     sortData(orgData, [-1, 1]);
     // 정렬선택박스 내림차순으로 변경하기
-    $('#sel').val('0');
+    $("#sel").val("0");
   } //////// if ////////
 
   /********************************************** 
@@ -175,7 +181,7 @@ export function Board() {
     // 내림차순 정렬 함수호출
     // sortData(orgData,[-1,1]);
 
- // 시작값 : (페이지번호-1)*블록단위수
+    // 시작값 : (페이지번호-1)*블록단위수
     let initNum = (pgNum - 1) * pgBlock;
     // 한계값 : 블록단위수*페이지번호
     let limitNum = pgBlock * pgNum;
@@ -243,8 +249,8 @@ export function Board() {
     const pgBlockCnt = Math.floor(limit / pgPgBlock);
     const pgBlockPad = limit % pgPgBlock;
     const pgLimit = pgBlockCnt + (pgBlockPad === 0 ? 0 : 1);
-    
-    console.log('페이징의 페이징한계 값',pgLimit);
+
+    console.log("페이징의 페이징한계 값", pgLimit);
 
     // // console.log(
     //   "블록개수:",
@@ -258,8 +264,6 @@ export function Board() {
     // [ 페이징의 페이지징 하기 ]
     // [1] 페이징 블록 - 한 페이징 블록수 : pgPgBlock변수(4)
     // [2] 페이징 현재 페이지번호 - pgPgNum 변수(기본값1)
-
-
 
     // 리액트에서는 jsx문법 코드를 배열에 넣고
     // 출력하면 바로 코드로 변환된다!!!
@@ -275,38 +279,47 @@ export function Board() {
 
     for (let i = initNum; i < limitNum; i++) {
       // 맨끝 페이지 번호보다 크면 나라가
-      if(i >= limit) break;
-      
-      {/* 1.페이징 링크 만들기 */}
+      if (i >= limit) break;
+
+      {
+        /* 1.페이징 링크 만들기 */
+      }
       pgCode[i] = (
         <Fragment key={i}>
           {pgNum - 1 === i ? (
             <b>{i + 1}</b>
-            ) : (
-              <a href="#" onClick={chgList}>
+          ) : (
+            <a href="#" onClick={chgList}>
               {i + 1}
             </a>
           )}
 
-          {// 바출력조건:
-          // 페이징의 페이징에서 끝번호 전번호일때와
-          // 동시에 전체 한계값이 전체페이지끝 이전번호 보다 작을때
-          (i < limitNum - 1 && i < limit-1) ? 
-          " | " : ""}
+          {
+            // 바출력조건:
+            // 페이징의 페이징에서 끝번호 전번호일때와
+            // 동시에 전체 한계값이 전체페이지끝 이전번호 보다 작을때
+            i < limitNum - 1 && i < limit - 1 ? " | " : ""
+          }
         </Fragment>
       );
     } ////// for /////
 
     // pgPgNum.current = 2;
 
-    { 
+    {
       // 2. 페이징 이전블록이동 버튼 - 배열 맨앞에 추가!
       // 기준: 1페이지가 아니면 보임!
-      pgCode.unshift (
-        pgPgNum.current === 1?
-        '':<Fragment key={-1}><a href="#">◀</a></Fragment>)
+      pgCode.unshift(
+        pgPgNum.current === 1 ? (
+          ""
+        ) : (
+          <Fragment key={-1}>
+            <a href="#">◀</a>
+          </Fragment>
+        )
+      );
     }
-    
+
     {
       // 3. 페이징 다음블록이동 버튼
       // 기준: 페이징의 페이징 블록 끝번호가 아니면 보임!
@@ -314,14 +327,19 @@ export function Board() {
         pgPgNum.current === pgLimit ? (
           ""
         ) : (
-        <Fragment key={-2}>
-          <a href="#" onClick={(e)=>{
-            e.preventDefault();
-            goPaging(1);
-          }}>▶</a>
+          <Fragment key={-2}>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                goPaging(1);
+              }}
+            >
+              ▶
+            </a>
           </Fragment>
         )
-     );
+      );
     }
 
     return pgCode;
@@ -332,7 +350,7 @@ export function Board() {
     // dir이동방향(오른쪽:+1, 왼쪽:-1)
     const newPgPgNum = pgPgNum.current + dir;
     // 새 페이지번호 : 전페이지 끝번호 + 1
-    const newPgNum = (newPgPgNum-1) * pgPgBlock + 1;
+    const newPgNum = (newPgPgNum - 1) * pgPgBlock + 1;
 
     // 페이징의 페이징번호 업데이트
     pgPgNum.current = newPgPgNum;
@@ -526,12 +544,15 @@ export function Board() {
         // let test = Math.max(1,2,3,4,5);
         // // console.log('1~5사이최대값:',test);
 
+        // 입력된 업로드 파일정보
+        console.log('업로드파일정보:',updateFile.current);
+
         // 4. 임시변수에 입력할 객체 데이터 생성하기
         let temp = {
           idx: maxNum + 1,
           tit: subEle.val().trim(),
           cont: contEle.val().trim(),
-          att: "",
+          att: uploadFile.current.name, //파일이름 업데이트
           date: `${yy}-${addZero(mm)}-${addZero(dd)}`,
           uid: logData.current.uid,
           unm: logData.current.unm,
@@ -544,6 +565,37 @@ export function Board() {
         orgTemp.push(temp);
 
         // // console.log("최종반영 전체데이터:",  orgTemp);
+
+        // [선택파일 서버전송]
+        if(updateFile.current){ //null아닌 할당상태일때만
+          // 원래는 form 태그로 싸여있어서 서버전송을 하지만
+          // 없어도 form 전송을 서버에 할 수 있는 객체가 있다!
+          // FormData() 클래스 객체임!!
+          const formData = new FormData();
+          // 전송할 데이터 추가하기
+          formData.append("file", fileInfo);
+  
+          // 폼데이터에는 키 값이 있음 확인하자!
+          for (const key of formData) console.log(key);
+  
+          // 서버전송은 엑시오스로 하자!
+          // server.js에 서버에서 post방식으로 전송받는
+          // 셋팅이 익스프레스에서 되어 있어야함!
+          // 첫번째 셋팅값 전송url에는 서버에 셋팅된
+          // path값과 같은 upload라는 하위 경로를 써준다!
+          // 두번째 셋팅값은 서버로 전송될 파일정보를 써준다!
+          axios
+            .post("http://localhost:8080/upload", formData)
+            .then((res) => {
+              // res는 성공결과 리턴값 변수
+              const { fileName } = res.data;
+              console.log("전송성공!", fileName);
+            })
+            .catch((err) => {
+              // err은 에러발생시 에러정보 변수
+              console.log("에러발생:", err);
+            });
+        } ////////////////// if //////////////////
 
         // 6. 로컬스에 반영하기
         localStorage.setItem("bdata", JSON.stringify(orgTemp));
@@ -965,7 +1017,9 @@ export function Board() {
               <tr>
                 <td>Attachment</td>
                 <td>
-                  <AttachBox />
+                  {/* 파일정보를 하위 컴포넌트에서 상위 컴포넌트 변수인
+                  uploadFile에 저장한다! */}
+                  <AttachBox saveFile={updateFileInfo}/>
                 </td>
               </tr>
             </tbody>
@@ -1162,7 +1216,6 @@ export function Board() {
   );
 } //////////// Board 컴포넌트 /////////////
 
-
 ////////////////////////////////////////////////
 //업로드 기능 서브 컴포넌트 및 메서드 만들기///////
 ////////////////////////////////////////////////
@@ -1172,7 +1225,7 @@ const AttachBox = () => {
   // [상태관리변수] ////////////
   // 1. 드래그 또는 파일을 첨부할때 활성화여부 관리 변수
   // 값: true이면 화성화, false이면 비활성화
-  const [isOn,setIsOn] = useState(false);
+  const [isOn, setIsOn] = useState(false);
   // 2. 업로드 파일 정보 관리변수
   const [uploadedInfo, setUploadedInfo] = useState(null);
 
@@ -1182,10 +1235,10 @@ const AttachBox = () => {
   const controlDragLeave = () => setIsOn(false);
 
   // 드래그를 할때 dragOver 이벤트는 비활성화함!(필요가없음)
-  const controlDragOver = e => e.preventDefault();
+  const controlDragOver = (e) => e.preventDefault();
 
-  // 드롭이벤트 발생시 처리 메서드 
-  const controlDrop = e => {
+  // 드롭이벤트 발생시 처리 메서드
+  const controlDrop = (e) => {
     // 기본 드롭기능 막기
     e.preventDefault();
     // 드롭했으므로 비활성화 전환!
@@ -1200,31 +1253,13 @@ const AttachBox = () => {
     // 파일정보셋팅 메서드 호출!
     setFileInfo(fileInfo);
 
-    // 원래는 form 태그로 싸여있어서 서버전송을 하지만
-    // 없어도 form 전송을 서버에 할 수 있는 객체가 있다!
-    // FormData() 클래스 객체임!!
-    const formData = new FormData();
-    // 전송할 데이터 추가하기
-    formData.append("file", fileInfo);
+    // 서브밋 저장구역에서 파일정보를 사용하도록
+    // 상위 컴포넌트 변수인 uploadFile에 저장하는
+    // 함수인 updateFileInfo()를 호출하는 속성인
+    // saveFile() 속성함수를 사용하여 업데이트한다!
+    saveFile(FileInfo);
 
-    // 폼데이터에는 키 값이 있음 확인하자!
-    for(const key of formData) console.log(key);
-
-    // 서버전송은 엑시오스로 하자!
-    // server.js에 서버에서 post방식으로 전송받는
-    // 셋팅이 익스프레스에서 되어 있어야함!
-    // 첫번째 셋팅값 전송url에는 서버에 셋팅된
-    // path값과 같은 upload라는 하위 경로를 써준다!
-    // 두번째 셋팅값은 서버로 전송될 파일정보를 써준다!
-    axios
-    .post('http://localhost:8080/upload',formData)
-    .then(res=>{ // res는 성공결과 리턴값 변수
-      const {fileName} = res.data;
-      console.log('전송성공!',fileName); 
-    })
-    .catch(err=>{ // err은 에러발생시 에러정보 변수
-      console.log('에러발생:',err);
-    });
+    // 서버전송은 서브밋 버튼 클릭후 실행!!!
 
   }; /////////////// controlDrop 메서드 ///////////////
 
@@ -1232,21 +1267,39 @@ const AttachBox = () => {
   const setFileInfo = (fileInfo) => {
     // 전달된 객체값을 한번에 할당하는 방법(객체구조분해법)
     // 구조분해 할당을 하면 객체의 값이 담긴다!
-    const {name,size: byteSize,type} = fileInfo;
+    const { name, size: byteSize, type } = fileInfo;
     // const {name,size,type} = fileInfo;
-    const size = (byteSize/(1024*1024)).toFixed(2)+'mb';
+    const size = (byteSize / (1024 * 1024)).toFixed(2) + "mb";
     // console.log('전체값:',fileInfo);
     // console.log('name:',name);
     // console.log('size:',size);
     // console.log('type:',type);
 
     // 파일정보 상태관리 변수에 업데이트함!
-    setUploadedInfo({name,size,type});
+    setUploadedInfo({ name, size, type });
     // -> 변경시 리랜더링으로 업로드구역에 반영됨!
-
-
-
   }; //////////////// setFileInfo 메서드 ////////////////
+
+  
+  // 파일선택 입력창 클릭시 파일선택으로 상태가 변경될때
+  // 파일정보업데이트하기 함수 ////
+  const changeUpload = ({target}) =>{// target은 이벤트 타겟!
+    // 파일정보 읽어오기
+    const fileInfo = target.files[0];
+    console.log('클릭파일:',file);
+
+    // 파일정보셋팅 메서드 호출!
+    setFileInfo(fileInfo);
+
+    // 서브밋 저장구역에서 파일정보를 사용하도록
+    // 상위 컴포넌트 변수인 uploadFile에 저장하는
+    // 함수인 updateFileInfo()를 호출하는 속성인
+    // saveFile() 속성함수를 사용하여 업데이트한다!
+    saveFile(fileInfo);
+
+  }; /////////// changeUpload 함수 ///////////
+
+
 
 
   /* 
@@ -1257,34 +1310,34 @@ const AttachBox = () => {
     onDrop : 드래그 대상 영역 안으로 드롭될때 
   */
   // 리턴 코드 /////////////////////////////////
-  return(
-    <label className="info-view"
+  return (
+    <label
+      className="info-view"
       onDragEnter={controlDragEnter}
       onDragLeave={controlDragLeave}
       onDragOver={controlDragOver}
       onDrop={controlDrop}
     >
-      <input type="file" className="file" />
+      {/* 파일을 클릭하여 선택창이 뜰 때 파일을 선택하면
+      현재상태가 변경되기 때문에 onChage이벤트 속성을 씀! */}
+      <input type="file" className="file" onChange={changeUpload} />
       {
         // 업로드정보가 null이 아니면 파일정보 출력
         uploadedInfo && <FileInfo uploadedInfo={uploadedInfo} />
       }
       {
         // 업로드정보가 null이면 안내문자 출력
-        !uploadedInfo && 
-        <>
-          {/* 업로드안내 아이콘 */}
-          <UpIcon />
-          <p className="info-view-msg" >
-            Click or drop the file here.
-          </p>
-          <p className="info-view-desc" >
-            Up to 3MB per file
-          </p>
-        </>
+        !uploadedInfo && (
+          <>
+            {/* 업로드안내 아이콘 */}
+            <UpIcon />
+            <p className="info-view-msg">Click or drop the file here.</p>
+            <p className="info-view-desc">Up to 3MB per file</p>
+          </>
+        )
       }
     </label>
-  ); 
+  );
 }; ///////////// AttachBox 컴포넌트 /////////////
 
 /* 
@@ -1294,18 +1347,16 @@ Object.entries(obj) – [키, 값] 쌍을 담은 배열을 반환합니다.
 */
 
 // 파일정보를 보여주는 파일정보 컴포넌트 /////////////
-const FileInfo = ({uploadedInfo}) => (
+const FileInfo = ({ uploadedInfo }) => (
   <ul className="info-view-info">
     {console.log(Object.entries(uploadedInfo))}
-    {
-      Object.entries(uploadedInfo).map(([key,value])=>(
-        // 코드 넣는곳
-        <li key={key}>
-          <span className="info-key">😚{key} : </span>
-          <span className="info-value">{value}</span>
-        </li>
-      ))
-    }
+    {Object.entries(uploadedInfo).map(([key, value]) => (
+      // 코드 넣는곳
+      <li key={key}>
+        <span className="info-key">😚{key} : </span>
+        <span className="info-value">{value}</span>
+      </li>
+    ))}
   </ul>
 ); //////////////// FileInfo 컴포넌트 ////////////////
 
@@ -1315,10 +1366,11 @@ const UpIcon = () => (
   <svg>
     {/* viewBox가 중요함 */}
     <svg className="icon" x="0px" y="0px" viewBox="0 0 99.09 122.88">
-    <path fill="#000" d="M64.64,13,86.77,36.21H64.64V13ZM42.58,71.67a3.25,3.25,0,0,1-4.92-4.25l9.42-10.91a3.26,3.26,0,0,1,4.59-.33,5.14,5.14,0,0,1,.4.41l9.3,10.28a3.24,3.24,0,0,1-4.81,4.35L52.8,67.07V82.52a3.26,3.26,0,1,1-6.52,0V67.38l-3.7,4.29ZM24.22,85.42a3.26,3.26,0,1,1,6.52,0v7.46H68.36V85.42a3.26,3.26,0,1,1,6.51,0V96.14a3.26,3.26,0,0,1-3.26,3.26H27.48a3.26,3.26,0,0,1-3.26-3.26V85.42ZM99.08,39.19c.15-.57-1.18-2.07-2.68-3.56L63.8,1.36A3.63,3.63,0,0,0,61,0H6.62A6.62,6.62,0,0,0,0,6.62V116.26a6.62,6.62,0,0,0,6.62,6.62H92.46a6.62,6.62,0,0,0,6.62-6.62V39.19Zm-7.4,4.42v71.87H7.4V7.37H57.25V39.9A3.71,3.71,0,0,0,61,43.61Z"/>
-  </svg>
+      <path
+        fill="#000"
+        d="M64.64,13,86.77,36.21H64.64V13ZM42.58,71.67a3.25,3.25,0,0,1-4.92-4.25l9.42-10.91a3.26,3.26,0,0,1,4.59-.33,5.14,5.14,0,0,1,.4.41l9.3,10.28a3.24,3.24,0,0,1-4.81,4.35L52.8,67.07V82.52a3.26,3.26,0,1,1-6.52,0V67.38l-3.7,4.29ZM24.22,85.42a3.26,3.26,0,1,1,6.52,0v7.46H68.36V85.42a3.26,3.26,0,1,1,6.51,0V96.14a3.26,3.26,0,0,1-3.26,3.26H27.48a3.26,3.26,0,0,1-3.26-3.26V85.42ZM99.08,39.19c.15-.57-1.18-2.07-2.68-3.56L63.8,1.36A3.63,3.63,0,0,0,61,0H6.62A6.62,6.62,0,0,0,0,6.62V116.26a6.62,6.62,0,0,0,6.62,6.62H92.46a6.62,6.62,0,0,0,6.62-6.62V39.19Zm-7.4,4.42v71.87H7.4V7.37H57.25V39.9A3.71,3.71,0,0,0,61,43.61Z"
+      />
+    </svg>
   </svg>
 );
- ////////// UpIcon 컴포넌트 //////////
-
-
+////////// UpIcon 컴포넌트 //////////
